91788f6a944a567f81d1e75ad9097895d29c3de2
Mykola Horbenko <dark.winter20.50@gmail.com>
2023-11-05 22:30:34 +0100

    Chahged "arrray" to "array" in tests files

--- tests/HeapTest.java
+++ tests/HeapTest.java
@@ -8,28 +8,28 @@ public class HeapTest {
 
     @Test
     void twoDifferentArraysShouldEqual(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(7,5,10,2,4,6,1,3,8,9));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(7,5,10,2,4,6,1,3,8,9));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Heap.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        Heap.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 
     @Test
     void  sortingSortedArray(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Heap.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        arrayToSort = Heap.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 
     @Test
     void  sortingWithOnlyOneElementToSort(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(10,2,3,4,5,6,7,8,9,1));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(10,2,3,4,5,6,7,8,9,1));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Heap.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        arrayToSort = Heap.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 }
--- tests/InsertionTest.java
+++ tests/InsertionTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 class InsertionTest {
     @Test
     void twoDifferentArraysShouldEqual(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(7,5,10,2,4,6,1,3,8,9));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(7,5,10,2,4,6,1,3,8,9));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Insertion.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        arrayToSort = Insertion.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 
     @Test
     void  sortingSortedArray(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Insertion.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        arrayToSort = Insertion.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 
     @Test
     void  sortingWithOnlyOneElementToSort(){
-        ArrayList<Integer> arrrayToSort = new ArrayList<Integer>(Arrays.asList(10,2,3,4,5,6,7,8,9,1));
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(10,2,3,4,5,6,7,8,9,1));
         ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        arrrayToSort = Insertion.sort(arrrayToSort);
-        Assertions.assertEquals(sortedArray, arrrayToSort);
+        arrayToSort = Insertion.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
     }
 }
