c2034d2c427cd984bd92471c912c5f26026fe6f5
Mykola Horbenko <dark.winter20.50@gmail.com>
2023-11-07 10:02:44 +0100

    Added Tests for quicksort

---
+++ tests/QuickTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class QuickTest {
+    @Test
+    void twoDifferentArraysShouldEqual(){
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(7,5,10,2,4,6,1,3,8,9));
+        ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        Quick.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
+    }
+
+    @Test
+    void  sortingSortedArray(){
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        arrayToSort = Quick.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
+    }
+
+    @Test
+    void  sortingWithOnlyOneElementToSort(){
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>(Arrays.asList(10,2,3,4,5,6,7,8,9,1));
+        ArrayList<Integer> sortedArray = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        arrayToSort = Quick.sort(arrayToSort);
+        Assertions.assertEquals(sortedArray, arrayToSort);
+    }
+}
