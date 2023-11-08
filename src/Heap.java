54826b5b622181dbceadc79b943953c9839874a1
Mykola Horbenko <dark.winter20.50@gmail.com>
2023-11-08 19:38:52 +0100

    Fixed Heap Sort

--- src/Heap.java
+++ src/Heap.java
@@ -2,35 +2,64 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Heap {
-    private static void fix(ArrayList<Integer> heap, int index, int size){
-        int theGreatest;
+    private static int fix(ArrayList<Integer> heap, int index, int size){
+        int theGreatest = 0;
+        int theGreatestElement = heap.get(index);
         int child1;
         int child2;
-        if(index * 2 + 1 <= size - 1 && index * 2 + 2 <= size - 1){
-            theGreatest = index;
+        if(index * 2 + 2 <= size - 1){
             child1 = index * 2 + 1;
             child2 = index * 2 + 2;
 
-            if(heap.get(theGreatest) < heap.get(child1)){
-                theGreatest = child1;}
-            if(heap.get(theGreatest) < heap.get(child2)){
-                theGreatest = child2;}
+            if(theGreatestElement < heap.get(child1)){
+                theGreatest = child1;
+                theGreatestElement = heap.get(child1);}
 
-            Collections.swap(heap, index, theGreatest);
+            if(theGreatestElement < heap.get(child2)){
+                theGreatest = child2;
+                theGreatestElement = heap.get(child2);}
+
+            if(theGreatest != 0)
+                Collections.swap(heap, index, theGreatest);
         }
 
-        else{
+        else if(index * 2 + 1 <= size - 1){
             child1 = index * 2 + 1;
-            if(heap.get(index) < heap.get(child1))
+            if(heap.get(index) < heap.get(child1)){
                 Collections.swap(heap, index, child1);
+                return child1;}
+        }
+
+        return theGreatest;
+    }
+
+    public static void makeHeap(ArrayList<Integer> array){
+        int parent;
+        for (int i = 0; i < array.size(); i++){
+            for (int j = i; j >= 0; j = parent){
+                fix(array, j, array.size());
+                if(j % 2 != 0)
+                    parent = (j - 1)/2;
+                else
+                    parent = (j - 2)/2;
+            }
         }
     }
-    public static ArrayList<Integer> sort(ArrayList<Integer> array){
-        int heapSize = array.size();
-        int lastParent = (heapSize - 2) / 2;
 
-        for(;lastParent >= 0;lastParent --){
-            fix(array,lastParent,heapSize);
+    public static ArrayList<Integer> sort(ArrayList<Integer> array){
+        makeHeap(array);
+        int elementToFix;
+        boolean heapFixed;
+
+        for(int i = array.size(); i > 1;){
+            Collections.swap(array,0, i - 1);
+            i--;
+            elementToFix = 0;
+            do {
+                elementToFix = fix(array,elementToFix,i);
+            }
+            while (elementToFix != 0);
+
         }
 
         return array;
