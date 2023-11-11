79ce1ca929f0ef568ef6b04e54928570e821b71d
Mykola Horbenko <dark.winter20.50@gmail.com>
2023-11-11 15:55:49 +0100

    Added QuickSort

--- src/Quick.java
+++ src/Quick.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Quick {
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Quick {
+    public static int sort(ArrayList<Integer> array, int pivot, int size){
+        if(pivot < size) {
+            int left = pivot + 1;
+            int right = size - 1;
+            while (true) {
+                while (left < right && array.get(left) < array.get(pivot))
+                    left++;
+                while (array.get(right) > array.get(pivot))
+                    right--;
+                if (left < right) {
+                    Collections.swap(array, left, right);
+                    left++;
+                    right--;
+                } else break;
+            }
+
+            Collections.swap(array, right, pivot);
+
+            Quick.sort(array, pivot, right);
+            Quick.sort(array, right + 1, size);
+
+            return right;
+        }
+        return 0;
+    }
 }
